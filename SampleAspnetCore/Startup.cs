412173621e0852f0890dee337c5993c9c1f5c8c6
skyplusplus <skyplusplus@gmail.com>
2016-09-19 22:09:51 +0200

    first commit

---
+++ SampleAspnetCore/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+
+namespace Sample
+{
+    public class Startup
+    {
+		private static string _applicationPath = string.Empty;
+        private static string _contentRootPath = string.Empty;
+
+        public IConfigurationRoot Configuration { get; set; }
+        
+        public Startup(IHostingEnvironment env)
+        {
+			_applicationPath = env.WebRootPath;
+            _contentRootPath = env.ContentRootPath;
+
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+
+            // Enable Cors
+            services.AddCors();
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+                .AddJsonOptions(opts =>
+                {
+                    // Force Camel Case to JSON
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+			// Add MVC to the request pipeline.
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+
+            
+
+
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+		
+        }
+    }
+}
---
+++ SampleAspnetCore/gulpfile.js
@@ -0,0 +1,106 @@
+﻿/// <binding />
+
+const gulp = require('gulp');
+const del = require('del');
+const typescript = require('gulp-typescript');
+const tscConfig = require('./tsconfig.json');
+const sourcemaps = require('gulp-sourcemaps');
+const tslint = require('gulp-tslint');
+const tsconfig = require('tsconfig-glob');
+
+var tsProject = typescript.createProject('tsconfig.json');
+
+// source files
+var sourcePaths = {
+    root: 'Scripts',
+    rootAllFiles: 'Scripts/**/*',
+    ts: 'Scripts/app/**/*.ts',
+    excludeTS: '!Scripts/app/**/*.ts',
+
+    libs: [
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/typescript/lib/typescript.js',
+        'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.min.js',
+        'node_modules/moment/moment.js'
+    ],
+
+    lib_angular: [
+        'node_modules/@angular/**'    
+    ],
+
+    lib_rxjs:[
+        'node_modules/rxjs/**'
+    ]
+};
+
+var targetPaths = { 
+    root: 'wwwroot',
+    rootAllFiles: 'wwwroot/**/*',
+    app: 'wwwroot/app',
+    lib: 'wwwroot/lib',
+
+    lib_angular: 'wwwroot/lib/@angular/',
+    lib_rxjs: 'wwwroot/lib/rxjs/'
+};
+
+// clean the contents of the distribution directory
+gulp.task('clean', function () {
+    return del(targetPaths.rootAllFiles);
+});
+
+// copy static assets - i.e. non TypeScript compiled source
+gulp.task('copy:assets', ['clean'], function () {
+    return gulp.src([sourcePaths.rootAllFiles, sourcePaths.excludeTS])
+      .pipe(gulp.dest(targetPaths.root));
+});
+
+// copy dependencies
+gulp.task('copy:libs', ['clean'], function () {
+
+    gulp.src(sourcePaths.lib_angular).pipe(gulp.dest(targetPaths.lib_angular));
+    gulp.src(sourcePaths.lib_rxjs).pipe(gulp.dest(targetPaths.lib_rxjs));
+    return gulp.src(sourcePaths.libs).pipe(gulp.dest(targetPaths.lib));
+
+});
+
+// linting
+gulp.task('tslint', function () {
+    return gulp.src(sourcePaths.ts)
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report());
+});
+
+// TypeScript compile
+gulp.task('compile', ['clean', 'tsconfig-glob'], function () {
+
+    return tsProject
+    .src(tscConfig.files)
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tsProject))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(targetPaths.app));
+    
+});
+
+// update the tsconfig files based on the glob pattern
+gulp.task('tsconfig-glob', function () {
+    return tsconfig({
+        configPath: '.',
+        indent: 2
+    });
+});
+
+// Run browsersync for development
+gulp.task('watch', function () {
+    gulp.watch([sourcePaths.rootAllFiles], ['build']);
+});
+
+gulp.task('build', [ 'compile', 'copy:libs', 'copy:assets']);
+//DISABLED linting => gulp.task('build', ['tsconfig-glob', 'tslint', 'compile', 'copy:libs', 'copy:assets']);
+gulp.task('default', ['build']);
+
